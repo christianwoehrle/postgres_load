49e31b330c065e922d7f2e2fb27b81fb63f68227
Christian Woehrle <christian.woehrle@dwpbank.de>
2020-02-29 07:13:09 +0100

    additional logging

--- postgres-load.go
+++ postgres-load.go
@@ -15,19 +15,26 @@ func main() {
 	password := flag.String("password", "foo", "database password")
 	url := flag.String("url", "foo", "database url")
 	database := flag.String("database", "postgres", "database name")
+	// for param see https://godoc.org/github.com/lib/pq
 	param := flag.String("param", "sslmode=disable", "db-parameter like sslmode=disable")
 	flag.Parse()
 
 	connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?%s", *user, *password, *url, *database, *param)
-	// + "?" + "sslmode=verify-full"
 	for {
 		insert(connStr)
 	}
 }
 
 func insert(connStr string) {
+
+	log.Println("opening connection ...")
 	db, err := sql.Open("postgres", connStr)
-	defer db.Close()
+	log.Println("opened")
+	defer func() {
+		log.Println("closing connection ...")
+		db.Close()
+		log.Println("closed")
+	}()
 	if err != nil {
 		log.Println("error getting connection: " + err.Error())
 		return
@@ -36,12 +43,13 @@ func insert(connStr string) {
 		t := time.Now()
 		id := 0
 
+		log.Print("about to insert row ... ")
 		err = db.QueryRow("INSERT INTO id(created_at, updated_at) VALUES($1, $2 ) RETURNING id", t, t).Scan(&id)
 		if err != nil {
-			log.Println("Error inserting row")
+			log.Println("  --> error inserting row")
 			return
 		} else {
-			log.Printf("Row %d inserted: %s \n", id, t.String())
+			log.Printf(" --> row %d inserted: %s \n", id, t.String())
 		}
 	}
 }
